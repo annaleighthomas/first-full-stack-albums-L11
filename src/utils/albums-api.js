5cef618d4a3a125c502446f899d3f403d33440ec
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-10 21:06:37 -0400

    rending albums

--- src/utils/albums-api.js
+++ src/utils/albums-api.js
@@ -1,6 +1,8 @@
 import request from 'superagent';
 
-const URL = '/api/albums';
+// const URL = '/api/albums';
+
+const URL = 'https://powerful-fjord-09218.herokuapp.com/api/albums';
 
 export async function getAlbums() {
   const response = await request.get(URL);
